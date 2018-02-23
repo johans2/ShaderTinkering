149b8bff4be06852d6703c3e1e83c5bfbdb14116
Johan <johansvensson@outlook.com>
2018-02-23 20:08:23 +0100

    Start impl on comupteshader.

--- Assets/GrassRendering/GrassPhysics.cs
+++ Assets/GrassRendering/GrassPhysics.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GrassPhysics : MonoBehaviour {
-
-
+    
     public Transform trampleTransform;
 
     public Material grassMat;
@@ -21,11 +20,16 @@ public class GrassPhysics : MonoBehaviour {
 
         renderTex = new RenderTexture(512, 512, 24);
         renderTex.enableRandomWrite = true;
+        renderTex.wrapMode = TextureWrapMode.Clamp;
+        renderTex.useMipMap = false;
         renderTex.Create();
 
         shader.SetTexture(flashInputHandler, "Result", renderTex);
         shader.Dispatch(flashInputHandler, 512 / 8, 512 / 8, 1);
         shader.SetTexture(updateKernel, "Result", renderTex);
+        shader.SetFloat("width", 512);
+        shader.SetFloat("height", 512);
+
         grassMat.SetTexture("_TrampleTex", renderTex);
 
         if (debugMat != null)
@@ -36,7 +40,18 @@ public class GrassPhysics : MonoBehaviour {
 	
 	void Update () {
         shader.SetFloat("eTime", Time.timeSinceLevelLoad);
+
+        Vector4 tramplePos = (trampleTransform.position / 100f);
+        tramplePos.x += 0.5000000f;
+        tramplePos.z += 0.5000000f;
+
         
+        shader.SetVector("tramplePos", new Vector4(1 - tramplePos.x, tramplePos.y, 1 - tramplePos.z, 0));
+
+        Debug.Log(tramplePos.x + "   " + tramplePos.z);
+
         shader.Dispatch(updateKernel, 512 / 8, 512 / 8, 1);
+
+
 	}
 }
