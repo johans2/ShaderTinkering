c0327ffca974e71c2a20cf89efc574b0d5d34d0b
Johan <johansvensson@outlook.com>
2018-03-14 00:14:19 +0100

    Multiple tramplers! performance takes a hit.

--- Assets/GrassRendering/GrassPhysics.cs
+++ Assets/GrassRendering/GrassPhysics.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GrassPhysics : MonoBehaviour {
     
-    public Transform trampleTransform;
+    public Transform[] trampleTransforms;
 
     [Range(0.001f, 1f)]
     public float trampleCutoff = 0.1f;
@@ -21,10 +21,16 @@ public class GrassPhysics : MonoBehaviour {
 
     int updateKernel;
     private RenderTexture renderTex;
-    private ComputeBuffer imgBuffer;
-    private Vector2 previousPos;
+    private ComputeBuffer trampleBuffer;
+    private ComputeBuffer tramplerObjectsBuffer;
+
+    private Vector4[] tramplerData;
+    private Vector2[] previousPos;
 
     void Start () {
+        tramplerData = new Vector4[trampleTransforms.Length];
+        previousPos = new Vector2[trampleTransforms.Length];
+        
         // Create a rendertexture, for reading results.
         renderTex = new RenderTexture(512, 512, 24);
         renderTex.enableRandomWrite = true;
@@ -33,12 +39,12 @@ public class GrassPhysics : MonoBehaviour {
         
         // Create the compute buffer, for storing previous values.
         Vector4[] bufferData = new Vector4[texWidth * texHeight];
-        imgBuffer = new ComputeBuffer(bufferData.Length, 16); // 16 is 4 bytes for 4 floats
+        trampleBuffer = new ComputeBuffer(bufferData.Length, 16); // 16 is 4 bytes for 4 floats
         
         // Sets the texture and buffer for the update kernel.
         updateKernel = shader.FindKernel("UpdatePhysics");
         shader.SetTexture(updateKernel, "Result", renderTex);
-        shader.SetBuffer(updateKernel, "imgBuffer", imgBuffer);
+        shader.SetBuffer(updateKernel, "trampleBuffer", trampleBuffer);
         shader.SetFloat("width", texWidth);
         shader.SetFloat("height", texHeight);
         shader.SetFloat("trampleCutoff", trampleCutoff);
@@ -56,25 +62,31 @@ public class GrassPhysics : MonoBehaviour {
 	
 	void Update () {
 
-        // These calculations are used to get the correct UV values from the world positions. 
-        // Should be fixed later on.
-        Vector2 tramplePos = new Vector2(trampleTransform.position.x, trampleTransform.position.z);
-        tramplePos /= 100f;
-        tramplePos.x += 0.50000000f;
-        tramplePos.y += 0.50000000f;
-        Vector2 moveDir = tramplePos - previousPos;
-        float velocity = Vector3.Magnitude(moveDir);
+        for (int i = 0; i < trampleTransforms.Length; i++)
+        {
+            // These calculations are used to get the correct UV values from the world positions. 
+            // Should be fixed later on.
+            Vector2 tramplePos = new Vector2(trampleTransforms[i].position.x, trampleTransforms[i].position.z);
+            tramplePos /= 100f;
+            tramplePos.x += 0.50000000f;
+            tramplePos.y += 0.50000000f;
+            Vector2 moveDir = tramplePos - previousPos[i];
+            
+            tramplerData[i] = new Vector4(1f - tramplePos.x, 1f - tramplePos.y, moveDir.x, moveDir.y);
+            previousPos[i] = tramplePos;
+        }
         
-        // Update the trample position and the move direction.
-        shader.SetVector("tramplePos", new Vector2(1 - tramplePos.x, 1 - tramplePos.y));
-        shader.SetVector("moveDir", new Vector2(moveDir.x, moveDir.y));
+        tramplerObjectsBuffer = new ComputeBuffer(tramplerData.Length, 16); // 16 is 4 bytes for 4 floats
+        tramplerObjectsBuffer.SetData(tramplerData);
+        shader.SetBuffer(updateKernel, "tramplerObjects", tramplerObjectsBuffer);
 
         shader.Dispatch(updateKernel, texWidth / 8, texHeight / 8, 1);
 
-        previousPos = tramplePos;
+        
+        tramplerObjectsBuffer.Dispose();
 	}
 
     void OnDestroy() {
-        imgBuffer.Dispose();
+        trampleBuffer.Dispose();
     }
 }
---
+++ Assets/GrassRendering/TramplerBot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TramplerBot : MonoBehaviour {
+
+    public float moveSpeed;
+
+    CharacterController ctrl;
+
+    Vector3 target;
+
+    IEnumerator Start()
+    {
+        ctrl = GetComponent<CharacterController>();
+        ctrl.enabled = false;
+        yield return new WaitForEndOfFrame();
+        ctrl.enabled = true;
+        target = GetRandomPoint();
+    }
+
+    void Update()
+    {
+        if (!ctrl.enabled)
+        {
+            return;
+        }
+
+        if (Vector2.Distance(new Vector2(target.x, target.z), new Vector2(transform.position.x, transform.position.z)) < 1f)
+        {
+            target = GetRandomPoint();
+        }
+
+        Vector3 moveDir = Vector3.Normalize( target - transform.position) * moveSpeed;
+        
+        moveDir.y = -20f;
+
+        ctrl.Move(moveDir * Time.deltaTime);
+    }
+
+
+    Vector3 GetRandomPoint() {
+        return new Vector3(Random.Range(-40f, 40f), 0, Random.Range(-40f, 40f));
+    }
+}
