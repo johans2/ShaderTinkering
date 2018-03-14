33d9f6ca9f9e68aa6bea919140676d4b012b7c65
Johan <johansvensson@outlook.com>
2018-03-14 18:20:01 +0100

    Added a trampler model with anims from asset store.

--- Assets/GrassRendering/GrassPointCloudRenderer.cs
+++ Assets/GrassRendering/GrassPointCloudRenderer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
+//[ExecuteInEditMode]
 public class GrassPointCloudRenderer : MonoBehaviour {
 
     public MeshFilter filter;
--- Assets/GrassRendering/Trampler.cs
+++ Assets/GrassRendering/Trampler.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Trampler : MonoBehaviour {
 
     public float moveSpeed;
+    public GameObject character;
 
     CharacterController ctrl;
+    Animator anim;
 
 	IEnumerator Start () {
         ctrl = GetComponent<CharacterController>();
         ctrl.enabled = false;
         yield return new WaitForEndOfFrame();
         ctrl.enabled = true;
+        anim = GetComponentInChildren<Animator>();
 	}
 	
 	void Update () {
@@ -21,22 +24,27 @@ public class Trampler : MonoBehaviour {
         }
 
         Vector3 moveDir = new Vector3();
+        Vector3 rotation = Vector3.zero;
+        float speed = 0;
 
         if(Input.GetKey(KeyCode.UpArrow) ) {
-            moveDir.z = moveSpeed;
+            moveDir = transform.forward * moveSpeed;
+            speed = 1;
         }
         if(Input.GetKey(KeyCode.DownArrow)) {
-            moveDir.z = -moveSpeed;
+            moveDir = transform.forward * -moveSpeed;
+            speed = 1;
         }
         if(Input.GetKey(KeyCode.RightArrow)) {
-            moveDir.x = moveSpeed;
+            rotation = new Vector3(0, 2.5f, 0);
         }
         if(Input.GetKey(KeyCode.LeftArrow)) {
-            moveDir.x = -moveSpeed;
+            rotation = new Vector3(0, -2.5f, 0);
         }
 
         moveDir.y = -20f;
-
+        anim.SetFloat("Speed", speed);
+        ctrl.transform.Rotate(rotation);
         ctrl.Move(moveDir * Time.deltaTime);
     }
 }
